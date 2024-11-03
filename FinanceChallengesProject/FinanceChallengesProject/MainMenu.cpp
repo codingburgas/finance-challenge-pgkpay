e36a722a446c5b07fe6678befe8638fd0f03d727
MANikolov22 <MANikolov22@codingburgas.bg>
2024-11-03 21:41:34 +0200

    Fix MainMenu.cpp

--- FinanceChallengesProject/FinanceChallengesProject/MainMenu.cpp
+++ FinanceChallengesProject/FinanceChallengesProject/MainMenu.cpp
@@ -42,17 +42,3 @@ int MainMenu::HandleInput() {
     }
     return 0;  // No button clicked
 }
-MainMenu.h
-#pragma once
-#include "raylib.h"
-
-class MainMenu {
-public:
-    void Init();
-    void Draw();
-    int HandleInput(); // Returns 1 for Login, 2 for Sign Up, 0 for no action
-
-private:
-    Rectangle loginButton = { 250, 360, 150, 60 };
-    Rectangle signUpButton = { 450, 360, 150, 60 };
-};
