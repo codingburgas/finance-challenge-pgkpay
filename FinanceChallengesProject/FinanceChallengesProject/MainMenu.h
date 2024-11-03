0f0b1932cb4866da98bbe78bdb0a696ff2380c96
MANikolov22 <MANikolov22@codingburgas.bg>
2024-11-03 21:40:07 +0200

    Add MainMenu.h

--- FinanceChallengesProject/FinanceChallengesProject/MainMenu.h
+++ FinanceChallengesProject/FinanceChallengesProject/MainMenu.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "raylib.h"
+
+class MainMenu {
+public:
+    void Init();
+    void Draw();
+    int HandleInput(); // Returns 1 for Login, 2 for Sign Up, 0 for no action
+
+private:
+    Rectangle loginButton = { 250, 360, 150, 60 };
+    Rectangle signUpButton = { 450, 360, 150, 60 };
+};
