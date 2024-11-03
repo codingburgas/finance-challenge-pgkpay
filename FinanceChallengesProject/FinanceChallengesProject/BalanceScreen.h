db459f8a71306c79c467492759e3e083e9593311
VNMihaylov22 <132439922+VNMihaylov22@users.noreply.github.com>
2024-11-03 22:08:33 +0200

    BalanceScreen.h

--- FinanceChallengesProject/FinanceChallengesProject/BalanceScreen.h
+++ FinanceChallengesProject/FinanceChallengesProject/BalanceScreen.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "raylib.h"
+#include <string>
+
+class BalanceScreen {
+public:
+    void Init();
+    void Draw();
+    int HandleInput();
+
+private:
+    float balance = 1000.0f;  // Starting balance
+    std::string inputText;    // For the deposit/withdraw amount
+    bool isInputActive = false;
+
+    Rectangle inputField = { 310, 280, 180, 40 };
+    Rectangle depositButton = { 270, 360, 110, 40 };
+    Rectangle withdrawButton = { 420, 360, 110, 40 };
+};
